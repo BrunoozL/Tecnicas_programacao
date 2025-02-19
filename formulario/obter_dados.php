b2301021585894d9df9c9d77d36024ab281ee61e
Bruno Righi <brunorighi267@gmail.com>
2025-02-19 10:50:29 -0300

    inserindo um arquivo txt sobre a introducao da disciplina e aprendendo como coletar os dados inseridos no form

--- formulario/obter_dados.php
+++ formulario/obter_dados.php
@@ -1,3 +1,9 @@
 <?php
-	echo "O nome inserido é: $_GET["nome"]";
+	// VARIAVEIS GLOBAIS
+	// Utilizando o método POST, coletamos os dados com a variavel $_POST
+	// Utilizando o método GET, coletamos os dados com a variavel $_GET
+	
+	// As duas formas funcionam, a mais simples é a segunda opção
+	echo "O nome inserido é: " . $_GET["nome"] . "<br>";
+	echo "O nome inserido é: {$_GET["nome"]}<br>";
 ?>
