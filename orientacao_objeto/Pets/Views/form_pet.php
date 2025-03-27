d5b449bedaf4f4bb73f1478822c8b63c591e8fa2
Bruno Righi <brunorighi267@gmail.com>
2025-03-27 12:37:21 -0300

    inserindo as mensagens de erro e estilizando o formulário

--- orientacao_objeto/Pets/Views/form_pet.php
+++ orientacao_objeto/Pets/Views/form_pet.php
@@ -29,17 +29,6 @@
             $msg[3] = "Selecione uma das opções sugeridas";
             $erro = true;
         }
-
-        if(!$erro)
-        {
-            $pet = new Pet($_POST["nome"], $_POST["idade"], $_POST["cor"], $_POST["raca"]);
-
-            //Mostrando os dados
-            echo "Nome: {$pet->getNome()}<br>";
-            echo "Idade: {$pet->getIdade()}<br>";
-            echo "Cor: {$pet->getCor()}<br>";
-            echo "Raça: {$pet->getRaca()}<br>";
-        }
     }
 ?>
 
@@ -54,29 +43,50 @@
     <body>
         <form class="form_pet" action="#" method="POST">
             <h1>Pet</h1>
-            <label>Nome: </label>
-            <input type="text" name="nome">
-            <div class="erro"></div>
+            <br><br>
+            <div class="div_inputs">
+                <label>Nome: </label>
+                <input type="text" name="nome">
+                <div class="erro"><?php echo $msg[0]; ?></div>
+                <br>
 
-            <label>Idade: </label>
-            <input type="text" name="idade">
-            <div class="erro"></div>
+                <label>Idade: </label>
+                <input type="text" name="idade">
+                <div class="erro"><?php echo $msg[1]; ?></div>
+                <br>
 
-            <label>Cor: </label>
-            <input type="text" name="cor">
-            <div class="erro"></div>
+                <label>Cor: </label>
+                <input type="text" name="cor">
+                <div class="erro"><?php echo $msg[2]; ?></div>
+                <br>
 
-            <label>Raça: </label>
-            <select name="raca">
-                <option value="0">Escolha a raça</option>
-                <option>Pitbull</option>
-                <option>Lhasa</option>
-                <option>Yorkshare</option>
-                <option>Indefinido</option>
-            </select>
-            <div class="erro"></div>
+                <label>Raça: </label>
+                <select name="raca">
+                    <option value="0">Escolha a raça</option>
+                    <option>Pitbull</option>
+                    <option>Lhasa</option>
+                    <option>Yorkshare</option>
+                    <option>Indefinido</option>
+                </select>
+                <div class="erro"><?php echo $msg[3]; ?></div>
+            </div>
 
+            <br><br>
             <input type="submit" value="Enviar">
         </form>
+        <br>
     </body>
 </html>
+
+<?php
+        if(!$erro)
+        {
+            $pet = new Pet($_POST["nome"], $_POST["idade"], $_POST["cor"], $_POST["raca"]);
+
+            //Mostrando os dados
+            echo "Nome: {$pet->getNome()}<br>";
+            echo "Idade: {$pet->getIdade()}<br>";
+            echo "Cor: {$pet->getCor()}<br>";
+            echo "Raça: {$pet->getRaca()}<br>";
+        }
+?>
