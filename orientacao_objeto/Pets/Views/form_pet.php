672fa4d9cb7928d31c2b78fb2a96d45b3c5a1997
Bruno Righi <brunorighi267@gmail.com>
2025-03-31 09:33:35 -0300

    commit

--- orientacao_objeto/Pets/Views/form_pet.php
+++ orientacao_objeto/Pets/Views/form_pet.php
@@ -1,35 +1,68 @@
 <?php
-    require "../Models/PetClass.php";
 
-    $msg = array("", "", "", "");
-    $erro = false;
+require "../Models/PetClass.php";
 
-    if($_POST)
+$msg = array("", "", "", "");
+$erro = false;
+$nome = "";
+$idade = null;
+$cor = "";
+$raca = "";
+
+
+if($_POST)
+{
+    if(empty($_POST["nome"]))
     {
-        if(empty($_POST["nome"]))
-        {
-            $msg[0] = "Preencha o campo";
-            $erro = true;
-        }
-
-        if(empty($_POST["idade"]))
-        {
-            $msg[1] = "Preencha o campo";
-            $erro = true;
-        }
-
-        if(empty($_POST["cor"]))
-        {
-            $msg[2] = "Preencha o campo";
-            $erro = true;
-        }
-
-        if($_POST["raca"] == 0)
-        {
-            $msg[3] = "Selecione uma das opções sugeridas";
-            $erro = true;
-        }
+        $msg[0] = "Preencha o campo";
+        $erro = true;
     }
+    else
+    {
+        $nome = $_POST["nome"];
+    }
+
+    if(empty($_POST["idade"]))
+    {
+        $msg[1] = "Preencha o campo";
+        $erro = true;
+    }
+    else
+    {
+        $idade = $_POST["idade"];
+    }
+
+    if(empty($_POST["cor"]))
+    {
+        $msg[2] = "Preencha o campo";
+        $erro = true;
+    }
+    else
+    {
+        $cor = $_POST["cor"];
+    }
+
+    if($_POST["raca"] !== 0)
+    {
+        $raca = $_POST["raca"];
+    }
+    else
+    {
+        $msg[3] = "Selecione uma das opções sugeridas";
+        $erro = true;
+    }
+
+    if(!$erro)
+    {
+        $pet = new Pet($_POST["nome"], $_POST["idade"], $_POST["cor"], $_POST["raca"]);
+
+        //Mostrando os dados
+        echo "Nome: {$pet->getNome()}<br>";
+        echo "Idade: {$pet->getIdade()}<br>";
+        echo "Cor: {$pet->getCor()}<br>";
+        echo "Raça: {$pet->getRaca()}<br><br>";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -46,47 +79,34 @@
             <br><br>
             <div class="div_inputs">
                 <label>Nome: </label>
-                <input type="text" name="nome">
+                <input type="text" name="nome" tabindex="1" value="<?php echo $nome;?>">
                 <div class="erro"><?php echo $msg[0]; ?></div>
                 <br>
 
                 <label>Idade: </label>
-                <input type="text" name="idade">
+                <input type="text" name="idade" tabindex="2" value="<?php echo $idade;?>">
                 <div class="erro"><?php echo $msg[1]; ?></div>
                 <br>
 
                 <label>Cor: </label>
-                <input type="text" name="cor">
+                <input type="text" name="cor" tabindex="3" value="<?php echo $cor;?>">
                 <div class="erro"><?php echo $msg[2]; ?></div>
                 <br>
 
                 <label>Raça: </label>
-                <select name="raca">
+                <select name="raca" tabindex="4">
                     <option value="0">Escolha a raça</option>
-                    <option>Pitbull</option>
-                    <option>Lhasa</option>
-                    <option>Yorkshare</option>
-                    <option>Indefinido</option>
+                    <option value="<?php if($_POST["raca"] == 1){echo $raca;} ?>">Pitbull</option>
+                    <option value="<?php if($_POST["raca"] == 2){echo $raca;} ?>">Lhasa</option>
+                    <option value="<?php if($_POST["raca"] == 3){echo $raca;} ?>">Yorkshare</option>
+                    <option value="<?php if($_POST["raca"] == 4){echo $raca;} ?>">Indefinido</option>
                 </select>
                 <div class="erro"><?php echo $msg[3]; ?></div>
             </div>
 
             <br><br>
-            <input type="submit" value="Enviar">
+            <input type="submit" value="Enviar" tabindex="5">
         </form>
         <br>
     </body>
 </html>
-
-<?php
-        if(!$erro)
-        {
-            $pet = new Pet($_POST["nome"], $_POST["idade"], $_POST["cor"], $_POST["raca"]);
-
-            //Mostrando os dados
-            echo "Nome: {$pet->getNome()}<br>";
-            echo "Idade: {$pet->getIdade()}<br>";
-            echo "Cor: {$pet->getCor()}<br>";
-            echo "Raça: {$pet->getRaca()}<br>";
-        }
-?>
